22b14dca4c713284f7947db89ef954c2decbdf7f
gecomerce <financeiro@nfeagro.com.br>
2025-09-15 16:33:12 -0300

    update

--- js/index.js
+++ js/index.js
@@ -6,9 +6,12 @@ window.addEventListener('load', () => {
 
         const usuarioCorreto = "usuario1";
         const senhaCorreta = "123";
+        const id_user = "1";
 
         if (usuario === usuarioCorreto && senha === senhaCorreta) {
             localStorage.setItem("logado", "true");
+            localStorage.setItem("user", id_user);
+
             window.location.href = "./pages/home.html";
         } else {
             erro.textContent = "Usuário ou senha inválidos!";
