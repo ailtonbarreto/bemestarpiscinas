67d9c9bd9bdfde2fb895535a9e028551e511642b
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-10-09 21:59:38 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-// window.addEventListener('load', () => {
-//     document.getElementById("btn-entrar").addEventListener("click", function () {
-//         const usuario = document.getElementById("user").value;
-//         const senha = document.getElementById("password").value;
-//         const erro = document.getElementById("erro");
-
-//         const usuarioCorreto = "usuario1";
-//         const senhaCorreta = "123";
-//         const id_user = "1";
-
-//         if (usuario === usuarioCorreto && senha === senhaCorreta) {
-//             localStorage.setItem("logado", "true");
-//             localStorage.setItem("user", id_user);
-
-//             window.location.href = "./pages/home.html";
-//         } else {
-//             erro.textContent = "Usuário ou senha inválidos!";
-//         }
-//     });
-
-//     console.log("js carregado com sucesso!");
-
-// });
-
-
-
 window.addEventListener('load', () => {
     document.getElementById("btn-entrar").addEventListener("click", async function () {
         const usuario = document.getElementById("user").value;
