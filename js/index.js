0181a2ccd3d2b3eb1d730ce84de332f426201ff8
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-11-04 22:37:44 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
         erro.textContent = "";
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/login", {
+            const response = await fetch("https://bemestarpiscinas-api.onrender.com/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
